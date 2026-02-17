2ba0c38ff9ec97334505a976602a77623a2cc3f7
Asma <asmabentaherr@gmail.com>
2026-02-17 21:39:37 +0200

    Add a simple flask server to run UptimeRobot

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ import handlers.feedback
 
 from database.db import create_tables
 
+
+# ----- Flask Keep-alive server -----
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "CETSU Student Support bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+# ---- Start Everything ---- 
 create_tables()
 
 bot.infinity_polling()
