902876b8fd2b93c21c8d6d8e862a13529dd04342
Asma <asmabentaherr@gmail.com>
2026-02-18 15:46:48 +0200

    Add flask server again

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ import handlers.feedback
 
 from database.db import create_tables
 
+# ----- Flask Server -----
+from flask import Flask
+import threading
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "CETSU Student Support Bot is alive!"
+
+def run_web():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+# Start web server in seperate thread
+threading.Thread(target=run_web).start()
 
 # ---- Start Everything ---- 
 create_tables()
