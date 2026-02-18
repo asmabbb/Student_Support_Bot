074ca9967ecb5a223baaebfc862a0abfb3d98011
Asma <asmabentaherr@gmail.com>
2026-02-18 14:55:42 +0200

    Remove flask server from main.py and requirements.txt and prepare to deploy on Render

--- main.py
+++ main.py
@@ -6,25 +6,8 @@ import handlers.feedback
 from database.db import create_tables
 
 
-# ----- Flask Keep-alive server -----
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "CETSU Student Support bot is alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
 
 # ---- Start Everything ---- 
 create_tables()
 
-bot.infinity_polling()
+bot.infinity_polling(timeout=10, long_polling_timeout=5)
