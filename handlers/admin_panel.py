5345b37d8e8d702d017785f26dd02fcf21d4a6e2
Asma <asmabentaherr@gmail.com>
2026-02-19 23:31:26 +0200

    Add admin panel and fix main and keyboard handlers

---
+++ handlers/admin_panel.py
@@ -0,0 +1,37 @@
+from bot_instance import bot
+from config import ADMIN_ID
+from database.models import get_all_feedbacks
+from keyboards import main_keyboard
+
+
+
+
+
+# View All Feedbacks (Admins)
+@bot.message_handler(func=lambda message: message.text == "📊 View All Feedbacks")
+def view_all_feedback(message):
+
+    if message.from_user.id not in ADMIN_ID:
+        bot.send_message(message.chat.id, "Unauthorized.")
+        return
+    
+    feedbacks = get_all_feedbacks()
+
+    if not feedbacks:
+        bot.send_message(message.chat.id, "No feedbacks available.")
+        return
+    
+    text = ""
+    for fb in feedbacks:
+            text += f"👤 {fb[1]} ({fb[0]})\n📝 {fb[2]}\n📅 {fb[3]}\n\n"
+
+    bot.send_message(message.chat.id, text)
+
+
+
+
+
+# Back Button
+@bot.message_handler(func=lambda message: message.text == "🔙 Back to Main Menu")
+def back_to_main(message):
+    bot.send_message(message.chat.id, "Welcom back the main menu!", reply_markup=main_keyboard.main_menu)
--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -1,9 +1,11 @@
 from bot_instance import bot
 from keyboards import main_keyboard
+from config import ADMIN_ID
 
 
 # Start Handler
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, f"Hello {message.from_user.first_name}!\nWelcom to CETSU Student Support Bot.", reply_markup=main_keyboard.main_menu)
+    is_admin = message.from_user.id in ADMIN_ID
+    bot.send_message(chat_id, f"Hello {message.from_user.first_name}!\nWelcom to CETSU Student Support Bot.", reply_markup=main_keyboard.main_menu(is_admin))
