a7618d6296fa3eff1ea05ff8cedc40934bc7b683
Asma <asmabentaherr@gmail.com>
2026-02-20 23:56:39 +0200

    Update fallback handler

--- handlers/fallback.py
+++ handlers/fallback.py
@@ -6,4 +6,4 @@ from config import ADMIN_ID
 def fallback(message):
     chat_id = message.chat.id
     is_admin = message.from_user.id in ADMIN_ID
-    bot.send_message(chat_id, "🤖 I didn't understand that.\nPlease choose an option from the menu.", reply_markup=main_keyboard.main_menu(is_admin))
+    bot.send_message(chat_id, "🤖: لم أفهم ماذا تقصد!\nمن فضلك إختر خياراً من القائمة.", reply_markup=main_keyboard.main_menu(is_admin))
