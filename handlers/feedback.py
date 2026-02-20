cea821ea54cc1875bf300495460e910f5dc400d2
Asma <asmabentaherr@gmail.com>
2026-02-20 17:36:21 +0200

    fix bug in feedback section #2nd try

--- handlers/feedback.py
+++ handlers/feedback.py
@@ -86,7 +86,7 @@ def process_feedback(message):
         bot.send_message(chat_id, "Feedback mode cancelled.")
         return
     
-    if text.startswith("/") or text == "Feedback":
+    if text.startswith("/") or text in ["✍️ Submit Feedback", "📂 View My Feedback", "🔙 Back to Main Menu"]:
         bot.send_message(chat_id, "⚠️ You are in feedback mode!\nSend your feedback or type /cancel.")
         bot.register_next_step_handler(message, process_feedback)
         return
