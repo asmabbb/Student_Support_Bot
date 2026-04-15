56680233298ea1048e73f05eeae8db1dd8926c7a
Asma <asmabentaherr@gmail.com>
2026-04-15 19:33:46 +0200

    Updating the Students GroupChat link

--- keyboards/main_keyboard.py
+++ keyboards/main_keyboard.py
@@ -26,7 +26,7 @@ bots_menu.add(roadMap, comp_lost, syllabus)
 
 # Join Us Menu:
 announce = InlineKeyboardButton("الإعلانات", url="https://t.me/Ta3alm_ET")
-group_chat = InlineKeyboardButton("مجموعة الطلبة", url="https://t.me/etstudents_e")
+group_chat = InlineKeyboardButton("مجموعة الطلبة", url="https://t.me/+TSOG9IIKJ580ZTFk")
 
 join_us_menu = InlineKeyboardMarkup(row_width=2)
 join_us_menu.add(announce, group_chat)
