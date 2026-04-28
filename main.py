d952af8aec62732f743c5c21bd7421674a6b101c
Asma <asmabentaherr@gmail.com>
2026-04-28 11:36:33 +0200

    Remove flask from main.py

--- main.py
+++ main.py
@@ -9,26 +9,11 @@ import handlers.fallback
 from database.db import create_tables
 from database.models import reset_database_if_new_year
 
-# ----- Flask Server -----
-from flask import Flask
-import threading
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "CETSU Student Support Bot is alive!"
-
-def run_web():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
-
-# Start web server in seperate thread
-threading.Thread(target=run_web).start()
-
 # ---- Start Everything ---- 
 create_tables()
 
 reset_database_if_new_year()
+
+print("Starting Student Support Bot (polling mode)...")
 
 bot.infinity_polling(timeout=10, long_polling_timeout=5)
