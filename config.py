3769efeffe6cefb45e2be41229c8ede677913f74
Asma <asmabentaherr@gmail.com>
2026-04-28 13:08:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
+# import os
+
+
+# API_TOKEN = os.environ['BOT_TOKEN']
+# ADMIN_ID = list(map(int, os.environ.get('ADMIN_ID', '').split(',')))
+# DB_NAME = os.environ.get('DB_NAME', 'CETSU_Student_Support.db')
+
+
 import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
 
-API_TOKEN = os.environ['BOT_TOKEN']
-ADMIN_ID = list(map(int, os.environ.get('ADMIN_ID', '').split(',')))
-DB_NAME = os.environ.get('DB_NAME', 'CETSU_Student_Support.db')
+# Bot token
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Database connection URL
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# List of sub-admin Telegram IDs
+ADMIN_ID = list(map(int, os.getenv("ADMINS", "").split(","))) if os.getenv("ADMINS") else []
